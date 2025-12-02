cb7dc5b3b9d80e2d72678d32fded4d0a7eb81fae
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-12-02 13:46:09 +0100

    Added fitering by color on FilterByColor component

--- filterproducts/src/components/FilterByColor/FilterByColor.tsx
+++ filterproducts/src/components/FilterByColor/FilterByColor.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import data from "../../db.json";
 
 export const FilterByColor = () => {
+  const [filterColor, setFilterColor] = useState("");
+
+  const filteredByColor = data.products.filter((item) =>
+    item.color.toLowerCase().includes(filterColor.toLowerCase())
+  );
   return (
     <div className="FilterByColor">
       <h3>Products color</h3>
 
+      <form className="FilterPrice">
+        <div className="filterPriceContent">
+          <label>Filter by Name</label>
+          <input
+            type="text"
+            value={filterColor}
+            onChange={(e) => setFilterColor(e.target.value)}
+          />
+        </div>
+      </form>
+
       <div className="filteredProduct">
-        {data.products.map((item) => (
+        {filteredByColor.map((item) => (
           <p key={item.id} className="filterItem" style={{ color: item.color }}>
             {item.color}
           </p>
